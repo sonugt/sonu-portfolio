d1c4d258a1d26a3c42fb0000d5eddf7e8d7052da
Sonu Gupta <sonu72752@gmail.com>
2024-11-18 22:05:57 -0600

    new updates

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Sonu Gupta",
   profile: '/profile.jpg',
   designation: "Software Engineer",
-  description: "I'm Sonu, a first-generation college student pursuing a degree in Computer Science and Mathematics. I have over 2 years of experience as a Software Engineer, with expertise in AI/ML, Full-stack Development, Mobile Development, and Technical Product Management. When I am not on the computer, you can find me on a bike, at the table tennis board, or in the kitchen trying out a new recipe.",
+  description: "I'm Sonu, a Software Engineer with experience building Web and Mobile applications. 🚀. I greatly enjoy Android and Full Stack Dev.",
   email: 'sonukgupta2001@gmail.com',
   phone: '712-360-6080',
   address: 'Orange City, Iowa (Open to Relocate)',
@@ -10,5 +10,5 @@ export const personalData = {
   linkedIn: 'https://www.linkedin.com/in/dxsonu/',
   leetcode: "https://leetcode.com/u/Dxsonu/",
   devUsername: "dxsonu7",
-  resume: "https://drive.google.com/file/d/1oTS-Ziyv73fGRzEMtoLa0vCI_i0utZEo/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1SEAwxsuzgVjpsVeQnurJ8-rbnJ9zYvnz/view?usp=sharing"
 } 
